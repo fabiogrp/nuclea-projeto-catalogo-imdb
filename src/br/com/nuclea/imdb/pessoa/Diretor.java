7d552ff430a9d38df33cea296cc770b4afae469e
fabiogrp <85580544+fabiogrp@users.noreply.github.com>
2026-04-18 20:50:06 -0300

    criar classe Diretor

---
+++ src/br/com/nuclea/imdb/pessoa/Diretor.java
@@ -0,0 +1,13 @@
+package br.com.nuclea.imdb.pessoa;
+
+public class Diretor {
+    private String estilo;
+
+    public void setEstilo(String estilo){
+        this.estilo = estilo;
+    }
+
+    public String getEstilo(){
+        return this.estilo;
+    }
+}
