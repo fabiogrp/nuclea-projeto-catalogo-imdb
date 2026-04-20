3fc50ae00615a84101acec6455680d6e2bec1e3f
fabiogrp <85580544+fabiogrp@users.noreply.github.com>
2026-04-20 19:58:18 -0300

    ajustes na classe Diretor

--- src/br/com/nuclea/imdb/pessoa/Diretor.java
+++ src/br/com/nuclea/imdb/pessoa/Diretor.java
@@ -1,7 +1,24 @@
 package br.com.nuclea.imdb.pessoa;
 
 public class Diretor {
+    private int id;
+    private String nome;
     private String estilo;
+    private static int contador;
+
+    public Diretor(String nome, String estilo) {
+        this.nome = nome;
+        this.estilo = estilo;
+        this.id = ++contador;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
 
     public void setEstilo(String estilo){
         this.estilo = estilo;
@@ -9,5 +26,17 @@ public class Diretor {
 
     public String getEstilo(){
         return this.estilo;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    @Override
+    public String toString() {
+        return "Diretor{" +
+                "nome='" + nome + '\'' +
+                ", estilo='" + estilo + '\'' +
+                '}';
     }
 }
